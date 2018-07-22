adc1f1210fa3b86afb0e81214924127e0086fa24
yohom <382146139@qq.com>
2018-07-22 18:42:32 +0800

    PhysicalShape Demo不做了, 和PhysicalModel类似, 只是可以自定义path(clipper参数), clipper这部分内容以后再分析

--- lib/painting_nodes/physical_model_demo.dart
+++ lib/painting_nodes/physical_model_demo.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+///
+/// PhysicalModel和PhysicalShape类似, 只不过后者可以自定义path, 前者只能画圆角矩形
+///
 class PhysicalModelDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
